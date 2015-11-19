911a91e40f174125f48a2dfbebdfb00343344e70
Filip Hindemark <filiphindemark@gmail.com>
2015-11-19 23:09:18 +0100

    Added possibility to search for a person, as well as added a "not found page"

--- data/fake_data.js
+++ data/fake_data.js
@@ -1,6 +1,7 @@
 module.exports = [
     {
         'index' : '1',
+        'short_name' : 'kohl',
         'about' : 'Helmut Kohl',
         'text'  : '”Han äter kopiöst med smör – först en assiett; jag kan tänka mig att det är tio såna där smörklickar, tio gram styck som ligger där, konsumerar han snabbt. Och så tar han in en till, också den går åt.”',
         'video' : 'https://youtu.be/nEUTn1_dTQM?t=1m36s',
@@ -8,6 +9,7 @@ module.exports = [
     },
     {
         'index' : '2',
+        'short_name' : 'maud',
         'about' : 'Maud Olofsson',
         'text'  : '”Jag tyckte det var lågt, det oschysst, och det var av sån slag att jag inte kunde försvara mig. För min del är det nu så, att jag kan inte träffa Maud Olofsson, jag vill inte göra det, kan inte lita på henne, törs inte prata med henne. Vad kommer hon i framtiden påstå att jag sagt eller gjort? Så träffar jag Maud måste jag ha en person med mig.”',
         'video' : 'https://youtu.be/3K6NhPD9u-4?t=26s',
